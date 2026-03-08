91a26e0ec65f470838eee2656bf4b71fb06d4395
reasadamin <39085086+reasadamin@users.noreply.github.com>
2026-03-08 20:03:17 +0100

    Update provider.tf
    user info edit

--- my_lab/terraform/devcloud_core/provider.tf
+++ my_lab/terraform/devcloud_core/provider.tf
@@ -2,6 +2,6 @@ provider "oci" {
   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaalgb7b76kajvhthyh72ql5q7fyoisrotvfix2tfd2bbjwj6ljqfuq"
   user_ocid = "ocid1.user.oc1..aaaaaaaapbguhez5eq6dyedf6txfqkonxtxamj5hxwm3eqqcslvgmcnwauca" 
   private_key_path = "/home/shamrat/terraform_devcloud_cred/shamrat_tf_pvt.pem"
-  fingerprint = "12:f1:7b:65:cf:20:1d:e3:f2:fc:f1:cf:7f:15:68:e3"
+  fingerprint = "12:f1:7b:test:value:1d:e3:f2:fc:f1:cf:7f:15:68:e3"
   region = "ap-mumbai-1"
 }
